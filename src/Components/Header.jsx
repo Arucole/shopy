68d257c06e80d8087d1a95737efd54180b7fbbda
Arucole <96977950+Arucole@users.noreply.github.com>
2023-11-26 21:33:28 +0100

    master

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,22 +1,85 @@
 import React from "react";
 import "./Header.css";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 
 function Header() {
   return (
-    <div className="haeder">
-      {/*image logo*/}
-      <div>
-        <img
-          style={{ width: 120, height: 40, marginTop: 10 }}
-          className="image"
-          src="https://links.papareact.com/f90"
-        />
+    <>
+      <div className="haeder">
+        {/*image logo*/}
+        <div>
+          <img
+            style={{ width: 120, height: 40, marginTop: 10 }}
+            className="image"
+            src="https://links.papareact.com/f90"
+          />
+        </div>
+        {/* Search Bar */}
+        <div className="headerInputContainer">
+          <input
+            className="headerInput"
+            type="text"
+            placeholder="search Items or products"
+          />
+          <SearchOutlinedIcon
+            style={{ color: "white", marginLeft: 4, marginTop: 2 }}
+          />
+        </div>
+
+        <div>
+          <h4 className="headerText">Hello John</h4>
+          <h4 className="headerText">Accounts & Lists</h4>
+        </div>
+
+        <div>
+          <h4 className="headerText">Returns</h4>
+          <h4 className="headerText">& Orders</h4>
+        </div>
+
+        <div style={{ position: "relative" }}>
+          <ShoppingCartOutlinedIcon style={{ color: "white" }} />
+          <span
+            style={{
+              position: "absolute",
+              left: 14,
+              right: 14,
+              backgroundColor: "white",
+              width: 14,
+              height: 14,
+              borderRadius: 7,
+              textAlign: "center",
+              fontSize: 12,
+              fontWeight: 400,
+            }}
+          >
+            0
+          </span>
+        </div>
+
+        {/* place and number */}
+        <div>
+          <h4 className="headerText">Nigeria</h4>
+          <h4 className="headerText">34784123499</h4>
+        </div>
       </div>
-      {/* Search Bar */}
-      <div>
-        <input type="text" placeholder="search Items or products" />
+
+      {/* Bottom header part */}
+      <div className="headerBottom">
+        <MenuOutlinedIcon style={{ color: "white", paddingTop: 1 }} />
+        <p className="headerBottomText">Buy</p>
+        <p className="headerBottomText">Healthy Products</p>
+        <p className="headerBottomText">Sell</p>
+        <p className="headerBottomText">Baby</p>
+        <p className="headerBottomText">Health & LifeStyle</p>
+        <p className="headerBottomText">Prime Video</p>
+        <p className="headerBottomText">Super Sale</p>
+        <p className="headerBottomText">Offers</p>
+        <p className="headerBottomText">Exciting Offers</p>
+        <p className="headerBottomText">Subscribe</p>
       </div>
-    </div>
+    </>
   );
 }
 
---
+++ src/Components/ProductItem.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./ProductItem.css";
+
+function ProductItem({ item }) {
+  return (
+    <div className="productItem">
+      {/* image */}
+      <img
+        style={{
+          width: 200,
+          height: 200,
+          marginLeft: "auto",
+          marginRight: "auto",
+        }}
+        src={item.image}
+        alt="Product Image"
+      />
+      {/* title of product */}
+      <p>{item.title.length > 30 ? item.title.substr(0, 30) : item.title}</p>
+      {/* product description */}
+      <p>
+        {item.description.length > 60
+          ? item.description.substr(0, 60)
+          : item.description}
+      </p>
+      {/* Add to cart Button */}
+      <button className="productItemButton">Add To Cart</button>
+      {/* Buy Now Button */}
+      <button className="productItemBuy">Buy Now</button>
+    </div>
+  );
+}
+
+export default ProductItem;
